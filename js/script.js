52cdf2765e542c02bc52b66c18f3b22403523a46
Mirko <mirkocaccavone03@gmail.com>
2025-01-24 16:01:38 +0100

    [feat] add innerHTML

--- js/script.js
+++ js/script.js
@@ -10,21 +10,34 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
     const arrayList = response.data;
     console.log(arrayList);
 
-    // quindi estrapoliamo gli oggetti singoli dell'array  con un ciclo
+    // quindi estrapoliamo gli oggetti singoli dell'array con un ciclo
     for(i =0; i < arrayList.length; i++){
         console.log(arrayList[i]);
         
-    }
+        // poi da ogni oggetto prendiamo gli elementi che ci servono e li salviamo in una variabile
+        const {id, title, date, url} = arrayList[i];
+        console.log(id, title, date, url);
 
+        // aggiungiamo in pagina (usando innerHtml) quello che ci serve
+        container.innerHTML += `
+            <div class="card">
+                <div class="containerPhoto">
+                    <img src="./img/pin.svg" alt="" class="pinImg">
+                    <img src=${url} alt="photo" class="singleImg">
+                </div>
+
+                <div class="date">${date}</div>
+                <div class="text">${title.toUpperCase()}</div> 
+            </div>
+        `;  
+    };
 });
 
 
 
 
-// poi da ogni oggetto prendiamo gli elementi che ci servono 
-// e li salviamo in una variabile
 
-// aggiungiamo in pagina (usando innerHtml) quello che ci serve
+
 
 
 
