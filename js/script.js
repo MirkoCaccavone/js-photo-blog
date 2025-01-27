172666b5b14f4668b7a47fd0711a0b68a8424028
Mirko <mirkocaccavone03@gmail.com>
2025-01-27 10:46:07 +0100

    [fix] change comments in js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
             const { id, title, date, url } = arrayList[i];
             // console.log(id, title, date, url);
 
-            // aggiungiamo in pagina (usando innerHTML) quello che ci serve
+            // dichiariamo una variabile che contenga l'html da aggiungere
             const cardHtml = `
                 <div class="card">
                     <div class="containerPhoto">
@@ -34,26 +34,32 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
 
             console.log(cardHtml);
             
-
+            // aggiungiamo il contenuto in pagina usando innerHTML
             container.innerHTML += cardHtml;
         }
 
+        // richiamiamo la classe card aggiunta in precedenza salvandola in una costante
         const cards = document.querySelectorAll(".card");
 
+        // creiamo un ciclo per l'evento click
         cards.forEach(card => {
             card.addEventListener('click', () => {
 
-                // Trova l'immagine all'interno della card
-                const imageElement = card.querySelector('.singleImg'); // Trova immagine
-                const imageUrl = imageElement.src; // Estrai URL dell'immagine
+                // salviamo in una costante l'immagine all'interno della card
+                const imageElement = card.querySelector('.singleImg');
 
-                // Crea il contenuto dinamico del popup
+                // Estraiamo URL dell'immagine e salviamocelo
+                const imageUrl = imageElement.src;
+
+                // Creiamo il contenuto dinamico del popup da aggiungere in pagina
                 const popUpContent = `
                     <img src="${imageUrl}" alt="Popup Image" class="popup-image">
                 `;
 
+                // aggiungiamo il contenuto in pagina usando innerHTML
                 popUpImg.innerHTML = popUpContent;
 
+                // rimuoviamo la classe hidden per mostrare in contenuto al click
                 popUp.classList.remove('hidden');
             });
         })
@@ -65,6 +71,8 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
 
 // Chiudi il popup
 closePopUp.addEventListener('click', () => {
+    
+    // al click sul bottone aggiungiamo la classe hidden per nascondelo
     popUp.classList.add('hidden'); 
 });
 
